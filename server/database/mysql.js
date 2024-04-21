0a108ece7fee36763c5626b241bc75a6a24fb45f
olios123 <oliwier4444k@gmail.com>
2024-04-21 14:32:03 +0200

    Commit

--- server/database/mysql.js
+++ server/database/mysql.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2')
 const config = require('./../../config')
 
 const connection = mysql.createConnection({
-    host: 'HOST',
-    user: 'USER',
-    port: 3306,
-    database: 'DATABASE',
-    password: 'PASSWORD'
+    host: config.database.host,
+    user: config.database.user,
+    port: config.database.port,
+    database: config.database.database,
+    password: config.database.password
 })
   
 connection.connect((err) =>
